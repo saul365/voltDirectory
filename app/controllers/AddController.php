307274522bae4b2ac1fd537dd20e1584dd8e8b8d
saul365 <saulbh365@comunidad.unam.mx>
2020-01-06 22:41:04 -0600

    feat: adding files for another view and controller
    this new page will be the one where the user
    uploads new contacts.

--- app/controllers/AddController.php
+++ app/controllers/AddController.php
@@ -1,13 +1,11 @@
 <?php
 use Phalcon\Mvc\Controller;
 
-class IndexController extends Controller
+class AddController extends Controller
 {
     public function indexAction()
     {
-		
-      		$this->assets->addCss('css/index.css');
+      		//$this->assets->addCss('css/index.css');
       		$this->view->setVar('prueba', $prueba);
     }
 }
-
